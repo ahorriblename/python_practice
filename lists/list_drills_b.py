552b53011a48c94e5ef908d6f881d0cb11c1d071
ahorriblename <124475233+ahorriblename@users.noreply.github.com>
2025-07-14 11:02:59 -0700

    Add diagonal problem

--- lists/list_drills_b.py
+++ lists/list_drills_b.py
@@ -21,4 +21,57 @@ def get_scale():
            print(new_list[i][j], end = " ")
 
     return new_list
-get_scale()
+# get_scale()
+
+def create_diagonal_list(num):
+    new_list = [[0 for i in range(num)] for j in range(num)]
+    lower_diag = 0
+    upper_diag = 0
+    for i in range(num):
+        for j in range(num):
+            # print(str(i) + " " + str(j) + " = " + str(new_list[i][j]))
+            if i == j:
+                new_list[i][j] = 0
+                count = 1
+                # above = [i + 1][j], below = [i - 1][j], next[i][j + 1], prev[i][j - 1]
+                if i + 1 < num:
+                    new_list[i + 1][j] = 1
+                if i - 1 >= 0:
+                    new_list[i - 1][j] = 1
+                if j + 1 < num:
+                    new_list[i][j + 1] = 1
+                if j - 1 >= 0:
+                    new_list[i][j - 1] = 1
+                to_add = 2
+                # adj diagonal, [2][2] = [3][1], [4][0], [1][3], [0][4]
+                # [i + 1][j - 1], [i + 2][j - 2], [i - 1][j + 1], [i - 2][j + 2]
+                # count = 1, [i + count][j - count], [i - count][j + count]
+                # count += 1
+                while i + count < num and j - count >= 0 and j + count < num and j - count >= 0:
+                    new_list[i + count][j - count] = to_add
+                    new_list[i - count][j + count] = to_add
+                    to_add += 2
+                    count += 1
+            if i + 1 < num and j - 1 >= 0:
+                lower_diag = new_list[i + 1][j - 1]
+            if j + 1 < num and j - 1 >= 0:
+                upper_diag = new_list[i - 1][j + 1]
+            if new_list[i][j] == 1 and lower_diag == 1:
+                count = 1
+                to_add = 3
+                while i + count < num and j - count >= 0 and j + count < num and j - count >= 0:
+                    new_list[i - count][j + count] = to_add
+                    to_add += 2
+                    count += 1
+            if new_list[i][j] == 1 and upper_diag == 1:
+                count = 1
+                to_add = 3
+                while i + count < num and j - count >= 0 and j + count < num and j - count >= 0:
+                    new_list[i + count][j - count] = to_add
+                    to_add += 2
+                    count += 1
+
+    return new_list
+a_list = create_diagonal_list(7)
+for row in a_list:
+    print(*row)
