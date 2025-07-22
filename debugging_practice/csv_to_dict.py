1df6648f8b9cba2b388c538b9b20597bf702a7a4
ahorriblename <124475233+ahorriblename@users.noreply.github.com>
2025-07-21 19:56:09 -0700

    Add original code to csv_to_dict.py

--- debugging_practice/csv_to_dict.py
+++ debugging_practice/csv_to_dict.py
@@ -1,6 +1,26 @@
+"""
 def csv_to_dict(text):
     lines = text.split("\n")
     dkeys = []
+    output = {}
+    # Set up dictionary keys from the header row
+    for word in lines[0].split(","):
+        dkeys.append(word)
+    # Create dictionary for each individual line
+    for line_num in range(len(lines)):
+        sample = {}
+        line = lines[line_num].split(",")
+        for index in range(len(dkeys)):
+            sample[dkeys[index]] = line[index]
+        output.append(sample)
+    return output
+    
+"""
+
+def csv_to_dict(text):
+    lines = text.split("\n")
+    dkeys = []
+    # change output from dictionary to list
     output = []
     # Set up dictionary keys from the header row
     for word in lines[0].split(","):
@@ -13,7 +33,7 @@ def csv_to_dict(text):
             sample[dkeys[index]] = line[index]
         # output.append(sample), no append method for dictionaries
 
-        # do not append the first line, it is the headers
+        # do not append the first line, it's all headers
         if line_num != 0:
             output.append(sample)
     return output
